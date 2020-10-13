7c38e3e2c0ec8ace6029a7e7d00d11982f08f5bb
Your Name <javatrap2020@gmail.com>
2020-10-13 18:34:21 +0300

    lesson3_2

--- src/main/java/org/example/model/UserModel.java
+++ src/main/java/org/example/model/UserModel.java
@@ -1,4 +1,20 @@
 package org.example.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+
 public class UserModel {
+
+    private String name;
+    private String email;
+    private String password;
+    private int id;
+
 }
